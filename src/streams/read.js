fb4a3aeeba0986f41d5ea309425dd5fe628ebe68
Oleg Shkalikov <maska6293@yandex.ru>
2022-11-30 09:38:23 +0300

    feat: implement read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import { getDirName } from '../helpers.js';
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+import { join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const name = './files/fileToRead.txt';
+
+  const __dirname = getDirName(import.meta.url);
+
+  const source = join(__dirname, name);
+
+  const readStream = createReadStream(source, 'utf8');
+
+  let content = '';
+
+  readStream
+    .on('data', (chunk) => (content += chunk))
+    .on('error', (err) => console.log(err.message))
+    .on('end', () => stdout.write(`${content}\n`));
 };
 
 await read();
