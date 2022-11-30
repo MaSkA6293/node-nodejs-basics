e9897baf18e133267d975a2fb583909d854ee12e
Oleg Shkalikov <maska6293@yandex.ru>
2022-11-30 09:40:54 +0300

    feat: implement decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,35 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { getDirName, isFileExist } from '../helpers.js';
+import { createUnzip } from 'node:zlib';
+import { join } from 'path';
+import { EOL } from 'os';
+
 const decompress = async () => {
-    // Write your code here 
+  const destinationName = './files/fileToCompressUnzipped.txt';
+  const sourceName = './files/archive.gz';
+
+  const __dirname = getDirName(import.meta.url);
+
+  const destination = join(__dirname, destinationName);
+  const source = join(__dirname, sourceName);
+
+  try {
+    const isSourceExist = await isFileExist(source);
+
+    if (!isSourceExist)
+      throw new Error(
+        `Decompress operation failed, file ${sourceName} doesn't exist.${EOL}Try to npm run zip:compress first`
+      );
+
+    const unzip = createUnzip();
+
+    const readStream = createReadStream(source);
+    const writeStream = createWriteStream(destination);
+
+    readStream.pipe(unzip).pipe(writeStream);
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
 await decompress();
