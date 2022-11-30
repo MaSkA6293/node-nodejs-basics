ec636d3aa7c931a34b782737ef36a9d84064175a
Oleg Shkalikov <maska6293@yandex.ru>
2022-11-30 09:27:24 +0300

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { readFile } from 'node:fs/promises';
+import { getDirName } from '../helpers.js';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const name = 'files/fileToRead.txt';
+
+  const __dirname = getDirName(import.meta.url);
+
+  const pathToFile = path.join(__dirname, name);
+
+  try {
+    const data = await readFile(pathToFile, { encoding: 'utf-8' });
+    console.log(data);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
