3993587257a090a0160b97fddf9259f5646a5aa7
Oleg Shkalikov <maska6293@yandex.ru>
2022-11-30 09:26:02 +0300

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { writeFile } from 'node:fs/promises';
+import { getDirName } from '../helpers.js';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const __dirname = getDirName(import.meta.url);
+
+  const pathToFile = path.join(__dirname, '/files/fresh.txt');
+
+  const content = 'I am fresh and young';
+
+  try {
+    await writeFile(pathToFile, content, {
+      flag: 'wx',
+    });
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
