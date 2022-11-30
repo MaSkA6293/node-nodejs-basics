2fc5df82ca1bc1470f614c826f0fe10400390dd8
Oleg Shkalikov <maska6293@yandex.ru>
2022-11-30 09:44:39 +0300

    feat: implement child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,32 @@
+import { fork } from 'child_process';
+import { getDirName } from '../helpers.js';
+import { join } from 'path';
+import { EOL } from 'os';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const pathToChild = 'files/script.js';
+
+  const __dirname = getDirName(import.meta.url);
+
+  const childPath = join(__dirname, pathToChild);
+
+  const child = fork(childPath, [...args.split(' ')], {
+    silent: true,
+  });
+
+  process.stdin.on('data', (msg) => {
+    console.log(`Send to the child process: ${msg.toString()}${EOL}`);
+    child.stdin.write(msg);
+  });
+
+  child.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  child.stderr.on('data', (data) => {
+    console.log(data.toString());
+    child.kill();
+  });
 };
 
-spawnChildProcess();
+spawnChildProcess('--name Jon --surname Doe --email dog@dog.com');
